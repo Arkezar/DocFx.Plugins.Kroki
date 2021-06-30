383757058ece56c0f846166c9f875c7e51d65806
Randolph, Jackson <agent00jackson@gmail.com>
2021-06-30 09:43:34 -0400

    Replace XML character codes with real character for render

--- DocFx.Plugins.Kroki/Renderers/KrokiRenderer.cs
+++ DocFx.Plugins.Kroki/Renderers/KrokiRenderer.cs
@@ -28,8 +28,17 @@ namespace DocFx.Plugins.Kroki
     {
       var formatter = GetFormatter(_settings.OutputFormat, renderer.Options, DiagramType);
       var client = new KrokiClient(_settings.ServiceUrl);
-      var byteData = client.GetDiagram(new KrokiPayload(token.Code, DiagramType, _settings.OutputFormat)).Result;
+      var code = CodeCleanup(token.Code);
+      var byteData = client.GetDiagram(new KrokiPayload(code, DiagramType, _settings.OutputFormat)).Result;
       return formatter.FormatDiagramData(byteData);
+    }
+
+    private static string CodeCleanup(string raw)
+    {
+      // Markdown in source /// comments are preprocessed to escape XML reserved characters.
+      return raw.Replace("&amp;", "&")
+               .Replace("&lt;", "<")
+               .Replace("&gt;", ">");
     }
 
     private KrokiFormatter GetFormatter(OutputFormat outputFormat, Options options, DiagramType diagramType)
